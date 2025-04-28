ba7b2071cb2cbb8a0512cab7cb9176ce67ba5a43
Rouge <14047458+rougetimelord@users.noreply.github.com>
2025-04-28 12:54:32 -0700

    Add face descriptions

--- faces.lua
+++ faces.lua
@@ -1,10 +1,10 @@
 local faces = {
-    one = {name="1", image = love.graphics.newImage("assets/img/1.png"), scoring = function (score) return score + 1 end},
-    two = {name="2", image = love.graphics.newImage("assets/img/2.png"), scoring = function (score) return score + 2 end},
-    three = {name="3", image = love.graphics.newImage("assets/img/3.png"), scoring = function (score) return score + 3 end},
-    four = {name="4", image = love.graphics.newImage("assets/img/4.png"), scoring = function (score) return score + 4 end},
-    five = {name="5", image = love.graphics.newImage("assets/img/5.png"), scoring = function (score) return score + 5 end},
-    six = {name="6", image = love.graphics.newImage("assets/img/6.png"), scoring = function (score) return score + 6 end},
+    one = {name="1", image = love.graphics.newImage("assets/img/1.png"), scoring = function (score) return score + 1 end, description = "One"},
+    two = {name="2", image = love.graphics.newImage("assets/img/2.png"), scoring = function (score) return score + 2 end, description = "Two"},
+    three = {name="3", image = love.graphics.newImage("assets/img/3.png"), scoring = function (score) return score + 3 end, description = "Three"},
+    four = {name="4", image = love.graphics.newImage("assets/img/4.png"), scoring = function (score) return score + 4 end, description = "Four"},
+    five = {name="5", image = love.graphics.newImage("assets/img/5.png"), scoring = function (score) return score + 5 end, description = "Five"},
+    six = {name="6", image = love.graphics.newImage("assets/img/6.png"), scoring = function (score) return score + 6 end, description = "Six"},
 }
 
 return faces
